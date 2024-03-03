53a1d52783540370d4d4147c99c2b2a612cf2ffa
Giovanni Bússola <giovanni.sbussola@gmail.com>
2024-03-03 09:53:10 -0300

    feat: add session use case

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,13 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
-import { prisma } from '@/lib/prisma'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
-  app.get('/users', async (request, reply) => {
-    const users = await prisma.user.findMany()
-
-    return reply.status(200).send({ users })
-  })
+  app.post('/sessions', authenticate)
 }
