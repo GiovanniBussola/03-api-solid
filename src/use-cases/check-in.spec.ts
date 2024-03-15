f8bdfdb4a0f4b952b96a0c12a2274f0d81bfe734
Giovanni Bússola <giovanni.sbussola@gmail.com>
2024-03-15 08:01:57 -0300

    feat: add method to validate distance

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -9,11 +9,11 @@ let checkinsRepository: InMemoryCheckInsRepository
 let gymsRepository: InMemoryGymsRepository
 
 describe('CheckInsUseCase', () => {
-  const makeRequest = () => ({
+  const makeRequest = ({ userLatitude = 0, userLongitude = 0 }) => ({
     gymId: 'gym-01',
     userId: 'user-01',
-    userLatitude: 0,
-    userLongitude: 0,
+    userLatitude,
+    userLongitude,
   })
 
   beforeEach(() => {
@@ -41,26 +41,48 @@ describe('CheckInsUseCase', () => {
   it('should be able to check in', async () => {
     vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
 
-    const { checkIn } = await sut.execute(makeRequest())
+    const { checkIn } = await sut.execute(makeRequest({}))
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
 
   it('should not be able to check in twice in the same day', async () => {
     vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
-    await sut.execute(makeRequest())
+    await sut.execute(makeRequest({}))
 
-    await expect(sut.execute(makeRequest())).rejects.toBeInstanceOf(Error)
+    await expect(sut.execute(makeRequest({}))).rejects.toBeInstanceOf(Error)
   })
 
   it('should be able to check in twice but in different days', async () => {
     vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
-    await sut.execute(makeRequest())
+    await sut.execute(makeRequest({}))
 
     vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
 
-    const { checkIn } = await sut.execute(makeRequest())
+    const { checkIn } = await sut.execute(makeRequest({}))
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym ', async () => {
+    gymsRepository.items.push({
+      id: 'gym-01',
+      description: '',
+      latitude: new Decimal(-23.3801301),
+      longitude: new Decimal(-53.2851249),
+      phone: '',
+      title: 'Any Academy',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await expect(
+      sut.execute(
+        makeRequest({
+          userLatitude: -24.0,
+          userLongitude: -53.0,
+        }),
+      ),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
